5fbf1ce283d45768de8d4e671338568735352779
Zdravko Chorlev <zdravko.chorlev@sap.com>
2025-08-21 17:39:04 +0300

    Add simple Go service for testing purpose

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte("Hello World!"))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
 func main() {
-	fmt.Println("Hello World!")
-	err := http.ListenAndServe(":8555", nil)
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/", rootHandler)
+
+	err := http.ListenAndServe(":8090", nil)
 	if err != nil {
 		panic("ListenAndServe: " + err.Error())
 	}
